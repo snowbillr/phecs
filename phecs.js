e8d0586e6eec33777f94ec328ebb81fd39f06c17
Bill Reed <billnreed@gmail.com>
2020-02-29 16:58:29 -0500

    initial commit

---
+++ phecs.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+console.log(add(1, 2));
---
+++ src/phecs.ts
@@ -0,0 +1,7 @@
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+// import "core-js/fn/array.find"
+// ...
+
+import { add } from "./basic-math";
+
+console.log(add(1, 2))
